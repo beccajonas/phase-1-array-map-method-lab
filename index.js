ff049ce6d8f3af4bd99a5e593488373ad62910a0
Becca Jonas <beccajonas3@gmail.com>
2023-11-01 12:03:38 -0400

    phase-1-array-map-method-lab solution

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
-
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(sentence => sentence.split(' ').map(word => {
+  return word.charAt(0).toUpperCase() + word.slice(1);}).join(' '));
 }
